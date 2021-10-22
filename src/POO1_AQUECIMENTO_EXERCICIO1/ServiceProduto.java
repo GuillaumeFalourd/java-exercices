dd58bbf8e8e1c1a6e86a668b99eb833408287b03
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-22 19:41:01 -0300

    Criar lista de produtos no ServiceProduto

--- src/POO1_AQUECIMENTO_EXERCICIO1/ServiceProduto.java
+++ src/POO1_AQUECIMENTO_EXERCICIO1/ServiceProduto.java
@@ -1,4 +1,10 @@
 package POO1_AQUECIMENTO_EXERCICIO1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceProduto {
+    List <Produto> listaDeProdutos = new ArrayList<>();
+
+
 }
