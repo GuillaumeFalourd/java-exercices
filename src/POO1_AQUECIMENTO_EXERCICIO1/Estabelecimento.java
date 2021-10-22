bc46df10301e3b637048ce9d81d008f3b25314c4
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-22 19:33:02 -0300

    Moldar classe estabelecimento

--- src/POO1_AQUECIMENTO_EXERCICIO1/Estabelecimento.java
+++ src/POO1_AQUECIMENTO_EXERCICIO1/Estabelecimento.java
@@ -1,4 +1,29 @@
 package POO1_AQUECIMENTO_EXERCICIO1;
 
+import java.util.List;
+
 public class Estabelecimento {
+    private String nome;
+    private List<Produto> listaDeProdutos;
+
+    public Estabelecimento(String nome, List<Produto> listaDeProdutos) {
+        this.nome = nome;
+        this.listaDeProdutos = listaDeProdutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Produto> getListaDeProdutos() {
+        return listaDeProdutos;
+    }
+
+    public void setListaDeProdutos(List<Produto> listaDeProdutos) {
+        this.listaDeProdutos = listaDeProdutos;
+    }
 }
