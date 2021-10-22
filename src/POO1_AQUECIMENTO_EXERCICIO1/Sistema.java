595167f78cccdb0ffed69bf1e53532c07b994249
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-22 19:36:01 -0300

    Criar método capturarDados

--- src/POO1_AQUECIMENTO_EXERCICIO1/Sistema.java
+++ src/POO1_AQUECIMENTO_EXERCICIO1/Sistema.java
@@ -1,4 +1,10 @@
 package POO1_AQUECIMENTO_EXERCICIO1;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private static Scanner capturarDados (String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
