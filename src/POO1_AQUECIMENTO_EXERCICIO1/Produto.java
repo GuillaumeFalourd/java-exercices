915d170f72d545b4be0df39ef4a178d0fee3924a
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-22 19:33:55 -0300

    Moldar classe Produto

--- src/POO1_AQUECIMENTO_EXERCICIO1/Produto.java
+++ src/POO1_AQUECIMENTO_EXERCICIO1/Produto.java
@@ -1,4 +1,27 @@
 package POO1_AQUECIMENTO_EXERCICIO1;
 
 public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
