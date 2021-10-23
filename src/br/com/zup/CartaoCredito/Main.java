b0c57f1e0c7c2bcf64390b3063d467f02e42a29a
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-23 16:03:26 -0300

    criar construtor

--- src/br/com/zup/CartaoCredito/Main.java
+++ src/br/com/zup/CartaoCredito/Main.java
@@ -1,7 +1,7 @@
 package br.com.zup.CartaoCredito;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
     }
 }
--- src/br/com/zup/CartaoCredito/Sistema.java
+++ src/br/com/zup/CartaoCredito/Sistema.java
@@ -1,5 +1,12 @@
 package br.com.zup.CartaoCredito;
 
 public class Sistema {
+    public static void executar() throws Exception {
 
+        CartaoDeCredito exemplo = new CartaoDeCredito(500, 100, "Lurrégia");
+
+
+
+    }
 }
+
