a4fd9f23193ff247610eb2050178b38fc5edcfe8
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-23 15:01:55 -0300

    Criar método para entrada de dados

--- src/Exercicio3Aquecimento/Sistema.java
+++ src/Exercicio3Aquecimento/Sistema.java
@@ -1,4 +1,12 @@
 package Exercicio3Aquecimento;
 
+import java.util.Scanner;
+
 public class Sistema {
+    // método para receber os dados do usuário
+
+    private static Scanner entradaDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
