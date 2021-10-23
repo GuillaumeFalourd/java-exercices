e7bb2be3a8567b40aafc5303e19e919491ab02a2
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-23 15:10:36 -0300

    Criar classe cadidato

--- src/GerenciadorCandidatosVestibular/Candidato.java
+++ src/GerenciadorCandidatosVestibular/Candidato.java
@@ -1,2 +1,51 @@
-package GerenciadorCandidatosVestibular;public class Candidato {
+package GerenciadorCandidatosVestibular;
+
+public class Candidato {
+    private int numeroMatricula;
+    private double notaCandidato;
+    private String nomeCandidato;
+
+    public Candidato() {
+    }
+
+    public Candidato(int numeroMatricula, double notaCandidato, String nomeCandidato) {
+        this.numeroMatricula = numeroMatricula;
+        this.notaCandidato = notaCandidato;
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    public int getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(int numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public double getNotaCandidato() {
+        return notaCandidato;
+    }
+
+    public void setNotaCandidato(double notaCandidato) {
+        this.notaCandidato = notaCandidato;
+    }
+
+    public String getNomeCandidato() {
+        return nomeCandidato;
+    }
+
+    public void setNomeCandidato(String nomeCandidato) {
+        this.nomeCandidato = nomeCandidato;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retornaDados = new StringBuilder();
+        retornaDados.append("--------------------------");
+        retornaDados.append("Nome:  "+ nomeCandidato);
+        retornaDados.append("Matrícula: "+ numeroMatricula);
+        retornaDados.append("Nota: "+ notaCandidato);
+        retornaDados.append("--------------------------");
+        return retornaDados.toString();
+    }
 }
--- src/GerenciadorCandidatosVestibular/Main.java
+++ src/GerenciadorCandidatosVestibular/Main.java
@@ -1,2 +1,13 @@
-package GerenciadorCandidatosVestibular;public class Main {
+package GerenciadorCandidatosVestibular;
+/*
+2.Crie um programa para gerenciar uma lista de candidatos de um vestibular,
+cada candidato tem número de matrícula e uma nota.
+    No final do programa, o
+ usuário deve decidir se irá adicionar mais de um candidato, exibir todos os
+ candidatos cadastrados ou excluir um candidato pelo número de matrícula
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
 }
