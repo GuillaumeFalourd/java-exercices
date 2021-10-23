f5507ca5b379c681ec4da81bb7e87d36ea7e3571
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-23 16:15:52 -0300

    Criar menu de opções

--- src/GerenciadorCandidatosVestibular/Sistema.java
+++ src/GerenciadorCandidatosVestibular/Sistema.java
@@ -7,6 +7,14 @@ public class Sistema {
         System.out.println(mensagem);
         return new Scanner(System.in);
     }
+    public static void menuOpcoes(){
+        System.out.println("\n------------O que deseja fazer?------------");
+        System.out.println("[1] - Cadastrar novo candidato;");
+        System.out.println("[2] - Exibir candidatos cadastrados;");
+        System.out.println("[3] - Sair");
+        System.out.println("---------------------------------------------\n");
+        System.out.println("Digite a opção desejada:");
+    }
     public static Candidato cadastrarCandidato(){
         String nome = receberDados("Digite o nome do Candidato: ").nextLine();
         int numeroMatricula = receberDados("Digite o número da matrícula: ").nextInt();
@@ -14,5 +22,23 @@ public class Sistema {
 
         Candidato novoCandidato = ServicoCandidato.cadastrarCandidato(nome, numeroMatricula,nota);
         return novoCandidato;
+    }
+    public static void executar (){
+        boolean continuarExecutar = true;
+
+        System.out.println("Bem vindo ao Sistema de gerenciamento de Candidatos");
+        while (continuarExecutar){
+            menuOpcoes();
+            int opcao = receberDados("").nextInt();
+
+            if (opcao == 1){
+                Candidato novoCandidato = cadastrarCandidato();
+                System.out.println(novoCandidato);
+            }
+            if (opcao == 3){
+                continuarExecutar = false;
+                System.out.println("Até mais!");
+            }
+        }
     }
 }
