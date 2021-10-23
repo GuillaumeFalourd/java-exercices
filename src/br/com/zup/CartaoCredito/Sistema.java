8605b858c081d40bb74bf890ec62e8773931d056
Lurrégia Barreto <luhhbarreto@gmail.com>
2021-10-23 16:09:21 -0300

    atribuir os métodos em sistema

--- src/br/com/zup/CartaoCredito/Sistema.java
+++ src/br/com/zup/CartaoCredito/Sistema.java
@@ -5,7 +5,18 @@ public class Sistema {
 
         CartaoDeCredito exemplo = new CartaoDeCredito(500, 100, "Lurrégia");
 
+        //Aumentando limite
+        exemplo.aumentarLimite(600);
 
+        //Diminuindo limite
+        exemplo.diminuirLimite(100);
+
+        //Realizando compra
+        exemplo.realizaComprar(200);
+        System.out.println("-----");
+
+        //Fatura do cartão
+        exemplo.imprimeFatura();
 
     }
 }
