926dba974462df2138da4188b92f015b8fed7db5
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-23 14:58:57 -0300

    Modelar classe Prato

--- src/Exercicio3Aquecimento/Prato.java
+++ src/Exercicio3Aquecimento/Prato.java
@@ -1,4 +1,45 @@
 package Exercicio3Aquecimento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prato {
+
+    public class Prato {
+        private String nome;
+        private List<Ingrediente> ingredientes = new ArrayList<>();
+
+        public Prato() {
+        }
+
+        public Prato(String nome, List<Ingrediente> ingredientes) {
+            this.nome = nome;
+            this.ingredientes = ingredientes;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public List<Ingrediente> getIngredientes() {
+            return ingredientes;
+        }
+
+        public void setIngredientes(List<Ingrediente> ingredientes) {
+            this.ingredientes = ingredientes;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder retorno = new StringBuilder();
+            retorno.append("\n Nome do prato: " + nome);
+            retorno.append("\n Ingredientes: " + ingredientes);
+            return retorno.toString();
+        }
+    }
+
 }
