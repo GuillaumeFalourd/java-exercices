597576fd8fd7c61612d4e48c05e400cdefabcb9d
Larissa Novais <larinovais54@gmail.com>
2021-10-24 16:25:08 -0300

    adicionar estrutura de repetição ao contador

--- src/exercicio2lista1estruturaderepeticao/Questão2.java
+++ src/exercicio2lista1estruturaderepeticao/Questão2.java
@@ -5,5 +5,10 @@ public class Questão2 {
     public static void main(String[] args) {
 
       int contador = 100;
+
+      while (contador >= 1){
+          System.out.println(contador);
+          contador --;
+      }
     }
 }
