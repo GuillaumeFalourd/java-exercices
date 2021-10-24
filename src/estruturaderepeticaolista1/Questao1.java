0fc0326aef4c74ff77876e49f3fd4765effbe13e
Larissa Novais <larinovais54@gmail.com>
2021-10-24 14:57:51 -0300

    adicionar estrutura de repetição para contador contar de 1 a 100

--- src/estruturaderepeticaolista1/Questao1.java
+++ src/estruturaderepeticaolista1/Questao1.java
@@ -5,7 +5,11 @@ public class Questao1 {
 
     public static void main(String[] args) {
 
-      int contador = 1;
+        int contador = 1;
 
+        while (contador <= 100) {
+            System.out.println(contador);
+            contador++;
+        }
     }
 }
