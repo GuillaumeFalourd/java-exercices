3a8b220769ce013c168e69d5e41b1591ed09d3e0
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-24 11:40:39 -0300

    Modelar classe candidato.

--- src/Exercicio2Aquecimento/Candidato.java
+++ src/Exercicio2Aquecimento/Candidato.java
@@ -1,4 +1,49 @@
 package Exercicio2Aquecimento;
 
 public class Candidato {
+    private String nome;
+    private String numeroMatricula;
+    private double nota;
+
+    public Candidato() {
+    }
+
+    public Candidato(String nome, String numeroMatricula, double nota) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.nota = nota;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public double getNota() {
+        return nota;
+    }
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("\n Nome: " + nome);
+        retorno.append("\n Número de matrícula: " + numeroMatricula);
+        retorno.append("\n Nota: " + nota);
+        return retorno.toString();
+    }
 }
--- src/Exercicio2Aquecimento/Main.java
+++ src/Exercicio2Aquecimento/Main.java
@@ -1,4 +1,7 @@
 package Exercicio2Aquecimento;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
