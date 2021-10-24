0c4c6968e39411f9ab630a4ebf06e01e131ac46d
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-24 11:48:39 -0300

    Criar funcionalidade para entrada de dados.

--- src/Exercicio2Aquecimento/Sistema.java
+++ src/Exercicio2Aquecimento/Sistema.java
@@ -1,5 +1,15 @@
 package Exercicio2Aquecimento;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    //método para a entrada de dados
+
+    private static Scanner entradaDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+
+    }
 
 }
