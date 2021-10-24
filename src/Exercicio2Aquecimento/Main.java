99ed7c2f3814e7e67246cc2a5795826cb694c19a
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-24 11:42:51 -0300

    Modelar classe service da classe Candidato.

--- src/Exercicio2Aquecimento/Main.java
+++ src/Exercicio2Aquecimento/Main.java
@@ -3,5 +3,6 @@ package Exercicio2Aquecimento;
 public class Main {
     public static void main(String[] args) {
 
+
     }
 }
--- src/Exercicio2Aquecimento/ServicoCandidato.java
+++ src/Exercicio2Aquecimento/ServicoCandidato.java
@@ -1,4 +1,27 @@
 package Exercicio2Aquecimento;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoCandidato {
+    private static List<Candidato> candidatos = new ArrayList<>();
+
+    public ServicoCandidato(List<Candidato> candidatos) {
+        this.candidatos = candidatos;
+    }
+
+    public List<Candidato> getCandidatos() {
+        return candidatos;
+    }
+
+    public void setCandidatos(List<Candidato> candidatos) {
+        this.candidatos = candidatos;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder retorno = new StringBuilder();
+        retorno.append("Candidatos: " + candidatos);
+        return retorno.toString();
+    }
 }
