688603d5b01cc6e514a8edde8873a10a0c7195e9
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-25 08:52:06 -0300

    Criar Métodos Construtores na Classe Conta Bancaria

--- src/POO1_Mod3_EXE2ContaBancaria/ContaBancaria.java
+++ src/POO1_Mod3_EXE2ContaBancaria/ContaBancaria.java
@@ -1,2 +1,38 @@
-package POO1_Mod3_EXE2ContaBancaria;public class ContaBancaria {
+package POO1_Mod3_EXE2ContaBancaria;
+
+public class ContaBancaria {
+    //Atributos
+    private String numeroConta;
+    private double saldo;
+    private String nomeCorrentista;
+
+    //Método Constructor
+    public ContaBancaria (String numeroConta, double saldo, String nomeCorrentista){
+
+    }
+    //Getter - Setter
+
+    public String getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(String numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public String getNomeCorrentista() {
+        return nomeCorrentista;
+    }
+
+    public void setNomeCorrentista(String nomeCorrentista) {
+        this.nomeCorrentista = nomeCorrentista;
+    }
 }
