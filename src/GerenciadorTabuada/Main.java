63992635dfdef8966ae9111a29142ff2aec384e3
Vanessa Lima de Barros <vanessalimab08@gmail.com>
2021-10-26 11:20:13 -0300

    Adicionar condição inteiros entre 1 e 10

--- src/GerenciadorTabuada/Main.java
+++ src/GerenciadorTabuada/Main.java
@@ -9,26 +9,30 @@ public class Main {
         int calculo = 0;
 
         while (continuar) {
-            System.out.println("Qual o número que deseja ver a tabuada:");
+            System.out.println("Escolha um número de 1 a 10 para ver a tabuada: ");
             int numero = entradaDados.nextInt();
             entradaDados.nextLine();
-
-            for (int i = 1; i < 11; i++) {
-                calculo = i * numero;
-                System.out.println(numero + " * " + i + " = " + calculo);
-            }
-            System.out.println("Deseja ver uma nova tabuada?");
-            System.out.println("[1] - Sim\n[2] - Não");
-            int opcao = entradaDados.nextInt();
-
-            if (opcao == 1){
-            }
-            else if (opcao == 2){
-                continuar = false;
-                System.out.println("Até mais!");
+            if (numero < 1 | numero > 10){
+                System.out.println("Digite um número entre 1 e 10");
             }
             else{
-                System.out.println("Opção inválida!");
+                for (int i = 1; i < 11; i++) {
+                    calculo = i * numero;
+                    System.out.println(numero + " * " + i + " = " + calculo);
+                }
+                System.out.println("Deseja ver uma nova tabuada?");
+                System.out.println("[1] - Sim\n[2] - Não");
+                int opcao = entradaDados.nextInt();
+
+                if (opcao == 1){
+                }
+                else if (opcao == 2){
+                    continuar = false;
+                    System.out.println("Até mais!");
+                }
+                else{
+                    System.out.println("Opção inválida!");
+                }
             }
         }
     }
