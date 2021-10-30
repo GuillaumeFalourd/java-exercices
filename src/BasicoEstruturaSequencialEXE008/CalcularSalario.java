50cee77055b8a19039fbd7730092ebaa7ced0c75
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-30 14:15:15 -0300

    Exibir calculo do salario

--- src/BasicoEstruturaSequencialEXE008/CalcularSalario.java
+++ src/BasicoEstruturaSequencialEXE008/CalcularSalario.java
@@ -28,6 +28,23 @@ public class CalcularSalario {
         //calcular salario liquido
         salarioLiquido = salarioBruto - descontos;
 
+        //Folha de Pagamento
+        System.out.println("===============================================");
+        System.out.println("                    Pagamento                  ");
+        System.out.println("===============================================");
+        System.out.println("Salario por hora: \t\t" + salarioPorHora);
+        System.out.println("Horas trabalhadas: \t\t" + horasTrabalhadas);
+        System.out.println("SALARIO BRUTO: \t\t" + salarioBruto);
+        System.out.println("===============================================");
+        System.out.println("DESCONTOS:");
+        System.out.println("Imposto de Renda (11%): \t" + impostoDeRenda);
+        System.out.println("INSS (8%): \t\t\t" + inss);
+        System.out.println("Sindicato (5%): \t\t" + sindicato);
+        System.out.println("TOTAL DESCONTOS: \t\t" + descontos);
+        System.out.println("===============================================");
+        System.out.println("SALARIO LIQUIDO: \t\t" + salarioLiquido);
+        System.out.println("===============================================");
+
 
     }
 
