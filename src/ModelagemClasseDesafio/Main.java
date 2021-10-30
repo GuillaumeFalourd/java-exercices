f1fa5e58edc0a71a82f4af58238e653f224968ed
Mariana Sousa Borges <m4r1-d3v@outlook.com>
2021-10-30 02:42:51 -0300

    Criar lista de países

--- src/ModelagemClasseDesafio/Main.java
+++ src/ModelagemClasseDesafio/Main.java
@@ -1,7 +1,13 @@
 package ModelagemClasseDesafio;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        List<Pais>continente = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
 
     }
 }
