a09c9033e9e28649970eb7a8893d3143e5571272
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-10-30 14:09:07 -0300

    Modelar variaveis para calculo

--- src/BasicoEstruturaSequencialEXE008/CalcularSalario.java
+++ src/BasicoEstruturaSequencialEXE008/CalcularSalario.java
@@ -1,4 +1,18 @@
 package BasicoEstruturaSequencialEXE008;
 
+import java.util.Scanner;
+
 public class CalcularSalario {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        double impostoDeRenda;
+        double inss;
+        double sindicato;
+        double salarioLiquido;
+
+
+
+    }
+
+
 }
